9ad82f4879fc136b09ce8bec14ff1255a1e2f4f4
Geetha A G <geethaag.official@gmail.com>
2025-01-12 14:50:25 +0530

    Update models.py

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    tmdb_id = models.IntegerField(unique=True) 
+    title = models.CharField(max_length=255)
+    overview = models.TextField()
+    release_date = models.DateField()
+    poster_path = models.URLField(null=True, blank=True)
+    genre = models.CharField(max_length=255, null=True, blank=True)  
+    rating = models.FloatField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.title
+
