6e7d77185ba4424ec9220ee34d113f706798b68f
Geetha A G <geethaag.official@gmail.com>
2025-02-13 17:58:45 +0530

    Update on source code

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,16 +1,16 @@
 import requests
 
-TMDB_API_KEY = '1f8cfbd7e214356d5ab236b766c6ba47'
+TMDB_API_KEY = 'TMDB API Key'
 TMDB_BASE_URL = 'https://api.themoviedb.org/3/'
 
 def popular_movies():
-    url = f"{'https://api.themoviedb.org/3/'}movie/popular?api_key={'1f8cfbd7e214356d5ab236b766c6ba47'}&language=en-US"
+    url = f"{'https://api.themoviedb.org/3/'}movie/popular?api_key={'TMDB API Key'}&language=en-US"
     response = requests.get(url)
     data = response.json()
     return data
 
 def top_rated_movies():  
-    url = f"{'https://api.themoviedb.org/3/'}movie/top_rated?api_key={'fbd7e214356d5ab236b766c6ba47'}&language=en-US"
+    url = f"{'https://api.themoviedb.org/3/'}movie/top_rated?api_key={'TMDB API Key'}&language=en-US"
     response = requests.get(url)
     data = response.json()
     return data
